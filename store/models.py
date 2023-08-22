f3a1b3a464b2f2a134de7cbcfda5318f97221299
abdulrahman253 <abdulrahmanheiba253@gmail.com>
2023-08-22 20:21:48 +0300

    p

--- store/models.py
+++ store/models.py
@@ -38,10 +38,7 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1 , choices=MEMBERSHIP_CHOICES , default= MEMBERSHIP_BRONZE )
-    class Meta :
-        indexes = [
-            models.Index(fields= ['last_name' , 'first_name'])
-        ]
+    
     
 class Order(models.Model):
     payment_pending = 'P'
